284533cda4727301a9196d969c55fcf90aa63776
Gleb <126592740+starl3t@users.noreply.github.com>
2025-05-26 22:34:04 +0500

    Update Task1.scala

--- homeworks/homework_5/src/main/scala/Task1.scala
+++ homeworks/homework_5/src/main/scala/Task1.scala
@@ -1,4 +1,3 @@
-/*
   Задание №1
   В задание уже описан тайп класс и синтакс для него.
   Вам необходимо в объекте ShowInstance описать инстансы тайп класса
@@ -10,10 +9,8 @@
   NormalCat - кот его_имя
   BigCat - большой кот его_имя
   VeryBigCat - очень большой кот его_имя
-
   Если кот будет в коробке, то к тому, что должно выводиться для кота
   необходимо добавить "в коробке". Если коробка пустая, то выводить "пустая коробка"
-
   В тестах можно всегда более точно посмотреть фразы.
  */
 object Task1 extends App {
@@ -26,33 +23,24 @@ object Task1 extends App {
   trait Show[-A] {
     def show(a: A): String
   }
-
-  sealed trait Cat {
-    def name: String
-  }
-  case class VeryLittleCat(name: String) extends Cat
-  case class LittleCat(name: String) extends Cat
-  case class NormalCat(name: String) extends Cat
-  case class BigCat(name: String) extends Cat
-  case class VeryBigCat(name: String) extends Cat
-
-  sealed trait Box[+A] {
-    def value: A
-  }
-  case class BoxWith[+A](value: A) extends Box[A]
-  case object EmptyBox extends Box[Nothing] {
-    override def value: Nothing = throw new Exception("Empty box!")
+@@ -45,9 +21,18 @@ object Task1 extends App {
   }
 
   object ShowInstance {
     implicit val catShow: Show[Cat] = ???
+    implicit val catShow: Show[Cat] = {
+      case VeryLittleCat(name) => s"очень маленький кот $name"
+      case LittleCat(name) => s"маленький кот $name"
+      case NormalCat(name) => s"кот $name"
+      case BigCat(name) => s"большой кот $name"
+      case VeryBigCat(name) => s"очень большой кот $name"
+    }
 
     implicit def boxShow[A: Show]: Show[Box[A]] = ???
+    implicit def boxShow[A: Show]: Show[Box[A]] = {
+      case EmptyBox => "пустая коробка"
+      case BoxWith(a) => s"${implicitly[Show[A]].show(a)} в коробке"
+    }
   }
 
   object ShowSyntax {
-    implicit class ShowOps[A](val a: A) {
-      def show(implicit show: Show[A]): String = show.show(a)
-    }
-  }
-}
