c445bf57421f1a510282b754bd3cb068787a94ea
Gleb <126592740+starl3t@users.noreply.github.com>
2025-05-26 23:54:17 +0500

    Update ResuourceTraining.scala

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -1,19 +1,21 @@
-package ru.dru
-
-import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
-
-import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
-
-
-/**
- * Необходимо реализовать функции readData и writeData, записывающие и читающие данные в/из файла соответственно.
- * В реализации следует применять безопасное использование ресурсов ZIO.acquireReleaseWith
- */
-
-
 object ResuourceTraining extends ZIOAppDefault {
 
   def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = ZIO.acquireReleaseWith(
+    ZIO.attempt(new BufferedReader(new FileReader(filePath)))
+  )(
+    reader => ZIO.succeed(reader.close())
+  ) {
+    reader => ZIO.attempt(reader.readLine())
+  }
+
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ZIO.acquireReleaseWith(
+    ZIO.succeed(new FileWriter(filePath))
+  )(
+    writer => ZIO.succeed(writer.close())
+  ) {
+    writer => ZIO.succeed(writer.write(data))
+  }
 
   def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
 
