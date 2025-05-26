4f15e51bfa1b2714d2c8b6d73bcd967929c11aef
Gleb <126592740+starl3t@users.noreply.github.com>
2025-05-26 22:34:43 +0500

    Update Task4.scala

--- homeworks/homework_5/src/main/scala/Task4.scala
+++ homeworks/homework_5/src/main/scala/Task4.scala
@@ -17,31 +17,37 @@ object Task4 extends App {
     def flatMap[A, B](fa: F[E, A])(f: A => F[E, B]): F[E, B]
 
     def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = ???
+    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = flatMap(fa)(a => pure(f(a)))
 
     def raiseError[A](fa: F[E, A])(error: => E):  F[E, A]
     def handleError[A](fa: F[E, A])(handle: E => A): F[E, A]
-  }
-
-  case class EIO[+E, +A](value: Either[E, A])
-  object EIO {
-    def apply[A](value: A): EIO[Nothing, A] = EIO[Nothing, A](Right(value))
+@@ -28,9 +18,27 @@ object Task4 extends App {
 
     def error[E, A](error: E): EIO[E, A] = EIO[E, A](Left(error))
 
     def possibleError[A](f: => A): EIO[Throwable, A] = ???
+    def possibleError[A](f: => A): EIO[Throwable, A] = {
+      Try(f) match {
+        case Success(value) => EIO(value)
+        case Failure(exception) => EIO.error(exception)
+      }
+    }
 
     implicit def monad[E]: MonadError[EIO, E] = ???
+    implicit def monad[E]: MonadError[EIO, E] = new MonadError[EIO, E] {
+      def pure[A](value: A): EIO[E,A] = EIO(value)
+      def flatMap[A, B](fa: EIO[E, A])(f: A => EIO[E, B]): EIO[E, B] =
+        fa.value match {
+          case Right(a) => f(a)
+          case Left(e) => EIO.error(e)
+        }
+      def raiseError[A](fa: EIO[E, A])(error: => E): EIO[E, A] = EIO.error(error)
+      def handleError[A](fa: EIO[E, A])(handle: E => A): EIO[E, A] =
+        fa.value match {
+          case Right(a) => fa
+          case Left(e) => EIO(handle(e))
+        }
+    }
   }
 
   object EIOSyntax {
-    implicit class EIOOps[E, A](val eio: EIO[E, A]) {
-      def flatMap[B](f: A => EIO[E, B]): EIO[E, B] =
-        EIO.monad[E].flatMap(eio)(f)
-
-      def map[B](f: A => B): EIO[E, B] = EIO.monad.map(eio)(f)
-
-      def handleError(f: E => A): EIO[E, A] =
-        EIO.monad.handleError(eio)(f)
-    }
-  }
-}
