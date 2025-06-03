f322a0e63ad2a9b91bb7c50db890b8eb26938258
Gleb <126592740+starl3t@users.noreply.github.com>
2025-06-03 21:29:08 +0500

    Update ResuourceTraining.scala

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -1,23 +1,21 @@
 object ResuourceTraining extends ZIOAppDefault {
 
   def readData(filePath: String): IO[Throwable, String] = ???
-  def readData(filePath: String): IO[Throwable, String] = ZIO.acquireReleaseWith(
-    ZIO.attempt(new BufferedReader(new FileReader(filePath)))
-  )(
-    reader => ZIO.succeed(reader.close())
-  ) {
-    reader => ZIO.attempt(reader.readLine())
-  }
-
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ZIO.acquireReleaseWith(
-    ZIO.succeed(new FileWriter(filePath))
-  )(
-    writer => ZIO.succeed(writer.close())
-  ) {
-    writer => ZIO.succeed(writer.write(data))
-  }
 
   def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(
+      ZIO.attempt(new BufferedReader(new FileReader(filePath))))(reader => ZIO.succeed(reader.close())) {
+      reader => ZIO.attempt(reader.readLine())
+    }
+  }
+
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith(
+      ZIO.succeed(new FileWriter(filePath)))(writer => ZIO.succeed(writer.close())) {
+      writer => ZIO.succeed(writer.write(data))
+    }
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
